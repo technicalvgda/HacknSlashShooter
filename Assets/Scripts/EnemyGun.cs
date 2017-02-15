ef72eeb31b1f0c2f3bfa2c4de95d216a48934aed
Cgumacal <chris.gumacal94@gmail.com>
2017-02-14 23:36:20 -0800

    comments

--- Assets/Scripts/EnemyGun.cs
+++ Assets/Scripts/EnemyGun.cs
@@ -3,27 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyGun : MonoBehaviour {
-    public float projectileWait = 1;
-    public float ProjectileSpeed = 1;
+    public float projectileWait = 1;//1 second intervals
+    public float ProjectileSpeed = 1;//shoots with force of 1 
     public GameObject ProjectilePrefab;
+
     private EnemyVision vision;
     private bool shooting;
     private GameObject player;
 
     // Use this for initialization
     void Start () {
-        vision = GetComponent<EnemyVision>();
-        shooting = false;
-        player = GameObject.FindGameObjectWithTag("Player");
+        vision = GetComponent<EnemyVision>(); // get vision component for the enemy
+        shooting = false; //initializes shooting boolean to not shooting
+        player = GameObject.FindGameObjectWithTag("Player"); //finds the player game object
 	}
 
     IEnumerator EnemyShoot()
     {
-        while (vision.sight)
+        while (vision.sight)//while enemy has sight continue to shoot
         {
-            Debug.Log("shoot");
-            Projectile.create(ProjectilePrefab, transform.gameObject, GetAngle(this.transform.position, player.transform.position), ProjectileSpeed);
-            yield return new WaitForSeconds(projectileWait);
+            Projectile.create(ProjectilePrefab, transform.gameObject, GetAngle(this.transform.position, player.transform.position), ProjectileSpeed); //fire bullet towards player
+            yield return new WaitForSeconds(projectileWait);// wait projectile wait time before firing again
         }
         shooting = false;
 
@@ -34,12 +34,12 @@ public class EnemyGun : MonoBehaviour {
         return Mathf.Atan2(v2.z - v1.z, v2.x - v1.x);
     }
 
-    public void startShooting()
+    public void startShooting()//function call to start coroutine
     {
-        if (!shooting)
+        if (!shooting)//if enemy is already shooting do nothing
         {
-            StartCoroutine(EnemyShoot());
-            shooting = true;
+            StartCoroutine(EnemyShoot()); //start shooting
+            shooting = true; //set shooting boolean to true
         }
     }
 }
--- Assets/Scripts/EnemyVision.cs
+++ Assets/Scripts/EnemyVision.cs
@@ -3,36 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyVision : MonoBehaviour {
-    public float viewDist = 5.0f;
-    public bool sight = false;
+    public float viewDist = 5.0f; //distance from enemy that the enemy will see the player 
+    public bool sight = false;//bool for if the enemy can see player 
 
-    
-
-    private RaycastHit _hit;
+    private RaycastHit _hit;//hit from raycast
     private Vector3 _playerLocation, _raycastDirection, _startVec;
     private GameObject _player;
 
 	// Use this for initialization
 	void Start () {
-        _player = GameObject.FindGameObjectWithTag("Player");
-        
+        _player = GameObject.FindGameObjectWithTag("Player");//find player game object
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        _startVec = this.transform.position;
-        _playerLocation = _player.transform.position;
+        _startVec = this.transform.position;//enemy position
+        _playerLocation = _player.transform.position;//player current position
 
-        _raycastDirection = _playerLocation - _startVec;
+        _raycastDirection = _playerLocation - _startVec;//raycast from the enemy towards the player
         //Debug.Log("something");
-        if(Vector3.Distance(_playerLocation, _startVec) < viewDist)
+        if(Vector3.Distance(_playerLocation, _startVec) < viewDist) //if the player is within vision range
         {
             //Debug.Log("player within range");
-            if (Physics.Raycast(_startVec, _raycastDirection, out _hit, viewDist) && _hit.collider.gameObject.transform.parent.tag == "Player")
+            if (Physics.Raycast(_startVec, _raycastDirection, out _hit, viewDist) && _hit.collider.gameObject.transform.parent.tag == "Player")//send raycast and if it hits the player object
             {
-               transform.LookAt(_player.transform.position);
+               transform.LookAt(_player.transform.position); // rotate enemy to look at the player
                sight = true;
-                GetComponent<EnemyGun>().startShooting();
+                GetComponent<EnemyGun>().startShooting();//start shooting at player
                //Debug.Log("I see the player"); 
             }
             else
