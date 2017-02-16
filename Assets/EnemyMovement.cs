75a06542c27a4d926af8c79398f85d9d3f9bec00
Timothy Touch <chaosbounder@gmail.com>
2017-02-16 15:20:54 -0800

    Enemy Movement
    Basic Enemy Movement, when they see you, they chase you to your last
    know spot

---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+	Transform player;
+	UnityEngine.AI.NavMeshAgent nav;
+
+
+	void Awake ()
+	{
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+		nav = GetComponent <UnityEngine.AI.NavMeshAgent> ();
+	}
+		
+	public void ChasePlayer()
+	{
+		nav.SetDestination (player.position); // Sets Enemy destination towards Player
+	}
+
+	// Use when Enemy or Player is Dead
+	public void StopChasingPlayer()
+	{
+		nav.enabled = false;
+	}
+}
