a86e07639d9010ed9dcce9f2e9e2e97774abfde6
Cgumacal <chris.gumacal94@gmail.com>
2017-03-09 15:09:18 -0800

    work on spawner
    spawner switched back to initialize and destroy
    
    Wave spawner started but doesnt work yet
    
    Switched spawners to work ontriggerenter so its only spawning when the
    player is in the area

--- Assets/Spawner.cs
+++ Assets/Spawner.cs
@@ -7,13 +7,55 @@ public class Spawner : MonoBehaviour {
     public GameObject enemy;
     public float spawnWait = 5;
     private float maxX, maxZ, minX, minZ;
+    public bool wave = false;
+    public int numberOfWaves = 1;
+    private int currentWave = 0;
+    private List<GameObject> WaveEnemies;
+    private GameObject player;
+    private List<GameObject> WaveList;
+
+
 	// Use this for initialization
 	void Start () {
+        WaveEnemies = new List<GameObject>();
+        player = GameObject.FindGameObjectWithTag("Player");
         maxX = transform.position.x + transform.localScale.x / 2;
         maxZ = transform.position.z + transform.localScale.z / 2;
         minX = transform.position.x - transform.localScale.x / 2;
         minZ = transform.position.z - transform.localScale.z / 2;
-        Timing.RunCoroutine(Spawn());
+        WaveList = new List<GameObject>();
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.tag == "Player")
+        {
+            if (wave)
+            {
+                Timing.RunCoroutine(WaveSpawn());
+            }
+            else
+            {
+                Timing.RunCoroutine(Spawn(), "spawn");
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.transform.tag == "Player")
+        {
+            if (wave)
+            {
+                WaveEnemies = new List<GameObject>();
+                Timing.RunCoroutine(WaveSpawn());
+            }
+            else
+            {
+                Timing.KillCoroutines("spawn");
+            }
+        }
     }
 	
 	// Update is called once per frame
@@ -25,10 +67,37 @@ public class Spawner : MonoBehaviour {
     {
         while (true)
         {
-            Instantiate(enemy, new Vector3(Random.Range(minX, maxX), 1, Random.Range(minZ, maxZ)), Quaternion.identity);
-            yield return Timing.WaitForSeconds(spawnWait);// wait projectile wait time before firing again
-
+            Vector3 spawnPoint = new Vector3(Random.Range(minX, maxX), 1, Random.Range(minZ, maxZ));
+            while(!(Vector3.Distance(player.transform.position, spawnPoint) < 5))
+            {
+                Instantiate(enemy, spawnPoint, Quaternion.identity);
+                yield return Timing.WaitForSeconds(spawnWait);// wait projectile wait time before firing again
+                break;
+            }
         }
+    }
+
+    IEnumerator<float> WaveSpawn()
+    {
+        do
+        {
+            Debug.Log("WaveSpawn start");
+            for (int i = 0; i < (5); i++)
+            {
+                WaveList.Add(Instantiate(enemy, new Vector3(Random.Range(minX, maxX), 1, Random.Range(minZ, maxZ)), Quaternion.identity));
+                Debug.Log("Spawn " + i);
+                Timing.WaitForSeconds(1);// wait projectile wait time before firing again
+                Debug.Log("return");
+            }
+            Debug.Log("exitted 4 loop");
+            //while (WaveList.Count > 0)
+            //{
+               // yield return Timing.WaitForSeconds(spawnWait);// wait projectile wait time before firing again
+            //}
+            currentWave++;
+            WaveList.Clear();
+        } while (currentWave != numberOfWaves);
+        yield return Timing.WaitForSeconds(spawnWait); 
 
     }
 }
