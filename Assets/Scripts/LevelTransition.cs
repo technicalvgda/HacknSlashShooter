9f01092905ae54ff3dd0cea9d552f341e05fd1b7
Joel Lee <lee.joel395@gmail.com>
2017-03-12 20:46:08 -0700

    DOTween Setup, Sliding Door, Level Transition
    Fixed our DOTween setup not beign completed.
    
    Added in a sliding door that can be oneway or bothways, locked and
    unlocked.
    
    Level Transitions work by making contact with an object that has the
    script,

---
+++ Assets/Scripts/LevelTransition.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using MovementEffects;
+using System.Collections.Generic;
+
+public class LevelTransition : MonoBehaviour {
+    public string levelToLoad;
+    public Text[] loadingText;
+    public Text textToDisplay;
+
+    //Load the scene
+    private IEnumerator<float> LoadNextLevel(string levelToLoad)
+    {
+        Timing.WaitForSeconds(1.0f);
+        AsyncOperation async = SceneManager.LoadSceneAsync(levelToLoad);
+        while (!async.isDone)
+        {
+            //if we're basically done, press a key to move to the next scene
+            if (async.progress >= 0.9f)
+            {
+                async.allowSceneActivation = true;
+            }
+            else
+            {
+                if (loadingText.Length > 0)
+                {
+                    Timing.RunCoroutine(CycleText(loadingText));
+                }
+            }
+            yield return 0f;
+        }
+    }
+
+    void OnTriggerEnter(Collider c)
+    {
+        if (c.gameObject.tag.Equals("Player"))
+        {
+            Timing.RunCoroutine(LoadNextLevel(levelToLoad));
+        }
+    }
+
+    private IEnumerator<float> CycleText(Text[] loadingText)
+    {
+        foreach(Text t in loadingText)
+        {
+            textToDisplay = t;
+            Timing.WaitForSeconds(1.0f);
+        }
+        yield return 0f;
+    }
+}
