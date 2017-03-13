c542020f2266450cc9e9a6fa6e614973ff5c22ec
Cgumacal <chris.gumacal94@gmail.com>
2017-03-13 10:51:27 -0700

    Finished continuous spawner
    Continuous spawner attatches to a trigger collider and while the player
    is within that collider it will continue to spawn enemies
    
    the bounds that the enemy is spawned in are the bounds of the trigger
    collider the script is attatched too but the enemy cannot spawn within 5
    units of the player (this keeps enemies from spawning on top of the
    player or so close that the player does not have time to react)
    
    enemy1 is the most likely to spawn
    enemy1spawn is the percent chance that it will spawn
    enemy2 is the medium likely to spawn
    enemy2spawn is the percent chance that it will spawn
    enemy3 is the least likely to spawn
    enemy3spawn is the percent chance that it will spawn
    
    enemy2 and enemy3 can be left blank if you only want the spawner to be
    able to spawn 1 or 2 enemy types only
    
    Wave spawner does not work, some variables are in place but have not
    finished implementing it yet.

--- Assets/Spawner.cs
+++ Assets/Spawner.cs
@@ -4,27 +4,41 @@ using UnityEngine;
 using MovementEffects;
 
 public class Spawner : MonoBehaviour {
-    public GameObject enemy;
-    public float spawnWait = 5;
-    private float maxX, maxZ, minX, minZ;
+    public GameObject enemy1;//most likely to spawn
+    public float enemy1spawn = 60f;
+    public GameObject enemy2;//medium likely to spawn
+    public float enemy2spawn = 25f;
+    public GameObject enemy3;//least likely to spawn 
+    public float enemy3spawn = 15f;
+    
+    public float spawnWait = 5;//time inbetween each spawn
+    private float maxX, maxZ, minX, minZ;//the bound in which the enemy can spawn
+
+    //Wave Spawn Variables
     public bool wave = false;
     public int numberOfWaves = 1;
     private int currentWave = 0;
-    private List<GameObject> WaveEnemies;
     private GameObject player;
     private List<GameObject> WaveList;
 
 
 	// Use this for initialization
 	void Start () {
-        WaveEnemies = new List<GameObject>();
         player = GameObject.FindGameObjectWithTag("Player");
         maxX = transform.position.x + transform.localScale.x / 2;
         maxZ = transform.position.z + transform.localScale.z / 2;
         minX = transform.position.x - transform.localScale.x / 2;
         minZ = transform.position.z - transform.localScale.z / 2;
         WaveList = new List<GameObject>();
-        
+        if(enemy2 == null)
+        {
+            enemy2spawn = 0;
+        }
+        if (enemy3 == null)
+        {
+            enemy3spawn = 0;
+        }
+
     }
 
     void OnTriggerEnter(Collider other)
@@ -33,7 +47,8 @@ public class Spawner : MonoBehaviour {
         {
             if (wave)
             {
-                Timing.RunCoroutine(WaveSpawn());
+                Debug.Log("Wave spawner not done yet");
+                //Timing.RunCoroutine(WaveSpawn());
             }
             else
             {
@@ -48,8 +63,7 @@ public class Spawner : MonoBehaviour {
         {
             if (wave)
             {
-                WaveEnemies = new List<GameObject>();
-                Timing.RunCoroutine(WaveSpawn());
+                
             }
             else
             {
@@ -68,9 +82,29 @@ public class Spawner : MonoBehaviour {
         while (true)
         {
             Vector3 spawnPoint = new Vector3(Random.Range(minX, maxX), 1, Random.Range(minZ, maxZ));
-            while(!(Vector3.Distance(player.transform.position, spawnPoint) < 5))
+            float spawn = Random.Range(0, (enemy1spawn + enemy2spawn + enemy3spawn));
+            while (!(Vector3.Distance(player.transform.position, spawnPoint) < 5))
             {
-                Instantiate(enemy, spawnPoint, Quaternion.identity);
+                
+                if(spawn < enemy1spawn)
+                {
+                    Instantiate(enemy1, spawnPoint, Quaternion.identity);
+                }
+                else if (spawn < enemy2spawn+enemy1spawn && enemy2 != null)
+                {
+                    Instantiate(enemy2, spawnPoint, Quaternion.identity);
+                }
+                else if (spawn < enemy3spawn + enemy2spawn + enemy1spawn && enemy3 != null)
+                {
+                    Instantiate(enemy3, spawnPoint, Quaternion.identity);
+                }
+                else
+                {
+                    Instantiate(enemy1, spawnPoint, Quaternion.identity);
+                }
+                
+
+               
                 yield return Timing.WaitForSeconds(spawnWait);// wait projectile wait time before firing again
                 break;
             }
@@ -84,7 +118,7 @@ public class Spawner : MonoBehaviour {
             Debug.Log("WaveSpawn start");
             for (int i = 0; i < (5); i++)
             {
-                WaveList.Add(Instantiate(enemy, new Vector3(Random.Range(minX, maxX), 1, Random.Range(minZ, maxZ)), Quaternion.identity));
+                //WaveList.Add(Instantiate(enemy, new Vector3(Random.Range(minX, maxX), 1, Random.Range(minZ, maxZ)), Quaternion.identity));
                 Debug.Log("Spawn " + i);
                 Timing.WaitForSeconds(1);// wait projectile wait time before firing again
                 Debug.Log("return");
