6393361853489f45eb0cd36141f284bd037b00da
Joel Lee <lee.joel395@gmail.com>
2017-03-18 01:06:03 -0700

    Added door interface, Implemented a door button
    Door button will open and/or unlock any door implementing the Door
    interface.

---
+++ Assets/DoorOpenButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpenButton : MonoBehaviour {
+    public bool unlockDoor; //do we want to keep the door locked and kept open? or do we want the door to be simply unlocked.
+    public GameObject door;
+    private Door d;
+
+    void Start()
+    {
+        d = door.GetComponentInChildren<Door>();
+    }
+
+    void OnTriggerStay(Collider c)
+    {
+        if (c.gameObject.tag == "Player" && Input.GetKeyDown(KeyCode.E))
+        {
+            d.Open();
+            if (unlockDoor)
+            {
+                d.Unlock();
+            }
+        }
+    }
+}
