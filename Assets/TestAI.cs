e27949ca752e57d76c07db0366d69f89d934cf7d
Joel Lee <lee.joel395@gmail.com>
2017-03-31 03:14:03 -0700

    InitialCommit - AI targetting priority

--- Assets/TestAI.cs
+++ Assets/TestAI.cs
@@ -10,24 +10,46 @@ public class TestAI : MonoBehaviour {
     private EnemyVision _vision;
     private EnemyGun _weapon;
 	private EnemyMovement _movement;
+    public string[] priorityTargetTag;
+    private GameObject _primary, _secondary, _tertiary, _current;
 	// Use this for initialization
 	void Start () {
         _vision = GetComponent<EnemyVision>();
         _weapon = GetComponent<EnemyGun>();
 		_movement = GetComponent<EnemyMovement> ();
+        _primary = GameObject.FindGameObjectWithTag(priorityTargetTag[0]);
+        _secondary = GameObject.FindGameObjectWithTag(priorityTargetTag[1]);
+        _tertiary = GameObject.FindGameObjectWithTag(priorityTargetTag[2]);
 	}
 	
+    //sets the priority target to the one specified
+    private void SetPriority(string target) {
+        _primary = GameObject.FindGameObjectWithTag(target);
+    }
+
 	// Update is called once per frame
 	void Update () {
-		if(_vision.alertness == 0)
-        {
-            //do nothing for now
+        if(_vision.CanSeeTarget(_primary) >= 1) {
+            _current = _primary;
+        }else if(_vision.CanSeeTarget(_secondary) >= 1) {
+            _current = _secondary;
+        }else if(_vision.CanSeeTarget(_tertiary) >= 1) {
+            _current = _tertiary;
+        }else {
+            _current = null;
         }
-        if(_vision.alertness >= 1)
-        {
-            transform.LookAt(_vision.personalLastSighting);
-			_movement.ChasePlayer ();
-            _weapon.startShooting();
+
+        if (_current != null) {
+            transform.LookAt(_current.transform.position);
+            _movement.ChaseTarget(_current);
+            if (_vision.CanSeeTarget(_current) >= 2) {
+                _weapon.startShooting();
+            }
         }
+        //check the priority target first
+        //if the priotity target is in range, skip the player check
+            //else check for the playe rinstead
+            //calling _vision.canseetarget and getting back the 1 return
+		
 	}
 }
