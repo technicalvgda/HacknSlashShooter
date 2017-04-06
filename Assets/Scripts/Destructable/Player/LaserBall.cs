5eb58c87cb05c61faa04af5143a244a8d10c0ff3
Joel Lee <lee.joel395@gmail.com>
2017-04-05 21:54:52 -0700

    Bugfix start
    Fixed enemy tags and SlowEnemy AI,
    Fixing being able to throw the decoy over walls.

--- Assets/Scripts/Destructable/Player/LaserBall.cs
+++ Assets/Scripts/Destructable/Player/LaserBall.cs
@@ -6,12 +6,10 @@ public class LaserBall : MonoBehaviour {
     public GameObject throwmarker;
     public GameObject decoy;
     private Vector3 _heightOfDecoy = new Vector3(0, 0.1f, 0);
-
-
-
-
     public GameObject bullet;
     private GameObject player;
+    private RaycastHit hit;
+    private Vector3 direction;
 	// Use this for initialization
 	void Start () {
         player = FindObjectOfType<PlayerController>().gameObject;
@@ -26,12 +24,23 @@ public class LaserBall : MonoBehaviour {
         }
         if (Input.GetButton("Fire2") && !GameObject.Find("Decoy(Clone)"))
         {
+            //TODO: make it so you can throw this over walls
             //Temporary markery effect thing, fix/change later
             //possible put this into a another script that handles the ability itself(not projectionpowerup.cs)
-            GameObject m = Instantiate(throwmarker, player.GetComponent<PlayerController>().GetMousePos() , transform.rotation);
-            Destroy(m, 0.5f);
-            GameObject d = Instantiate(decoy, transform.position, transform.rotation);
-            d.GetComponent<ProjectionPowerup>().Activate(m.transform.position + _heightOfDecoy);
+            Vector3 mousePos = player.GetComponent<PlayerController>().GetMousePos();
+            
+            direction = mousePos - player.transform.position;
+            if (Physics.Raycast(player.transform.position, direction, out hit, 7.5f))
+            {
+                Debug.DrawLine(player.transform.position, hit.point);
+                if (hit.collider.tag == "Ground")
+                {
+                    GameObject m = Instantiate(throwmarker, mousePos, transform.rotation);
+                    Destroy(m, 0.5f);
+                    GameObject d = Instantiate(decoy, transform.position, transform.rotation);
+                    d.GetComponent<ProjectionPowerup>().Activate(m.transform.position + _heightOfDecoy);
+                }
+            }
         }
 
     }
