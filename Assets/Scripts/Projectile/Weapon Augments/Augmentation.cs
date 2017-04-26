b02e8ec84fc9ed533f7bc6af58de5b5dbe35bc3c
Timothy Touch <chaosbounder@gmail.com>
2017-04-25 17:30:12 -0700

    Start of the Augmentations

---
+++ Assets/Scripts/Projectile/Weapon Augments/Augmentation.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Augmentation : MonoBehaviour 
+{
+	public abstract void augmentShot ();
+}
---
+++ Assets/Scripts/Projectile/Weapon Augments/KnockBackAugmentation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnockBackAugmentation : Augmentation
+{
+
+
+	private PlayerController _player;
+	private WeaponManager _playerWeapon;
+	// Use this for initialization
+	void Start ()
+	{
+		_player = GetComponentInParent<PlayerController> ();
+		_playerWeapon = GetComponentInParent<WeaponManager> ();
+	}
+
+	public override void augmentShot()
+	{
+		Debug.Log ("Augment shot");
+	}
+
+}
