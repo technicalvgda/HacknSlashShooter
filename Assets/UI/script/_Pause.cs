01d69e8bda72bd6d3b7192c35e8c1689e397ca8d
Doan D <tamitystg@gmail.com>
2017-05-04 15:45:53 -0700

    Main menu

---
+++ Assets/UI/script/_Pause.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _Pause : MonoBehaviour {
+
+    public GameObject pauseMenu;
+    public Animator pauseButtons;
+
+	void Update ()
+    {
+        if (pauseMenu != null && Input.GetKeyDown(KeyCode.Escape))
+        {
+            pauseMenu.SetActive(!pauseMenu.activeSelf);
+            if (pauseMenu.activeSelf)
+            {
+                Time.timeScale = 0;
+            }
+            else
+            {
+                Time.timeScale = 1;
+            }
+            pauseButtons.SetTrigger("Enter");
+        }
+	}
+}
